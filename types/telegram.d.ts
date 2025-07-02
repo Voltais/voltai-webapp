142982bca9a9884a840d78a54396eba92b5b0826
Voltais <stanban7@gmail.com>
2025-07-03 00:47:09 +0900

    Update telegram.d.ts

--- types/telegram.d.ts
+++ types/telegram.d.ts
@@ -1,1 +1,26 @@
+export {};
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        initData?: string;
+        initDataUnsafe?: {
+          user?: {
+            id: number;
+            first_name?: string;
+            last_name?: string;
+            username?: string;
+            language_code?: string;
+          };
+        };
+        close(): void;
+        expand(): void;
+        isExpanded: boolean;
+        sendData(data: string): void;
+        themeParams?: Record<string, any>;
+      };
+    };
+  }
+}
 
